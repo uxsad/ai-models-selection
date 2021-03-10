29e21587d40569161982d9f113b9e89d8593a7e5
Andrea Esposito <esposito_andrea99@hotmail.com>
2021-03-10 19:26:37 +0100

    Convert EFS to SciKit
    Changed exhaustive feature selection implementation to the one provided
    by mlxtend using SciKit.

--- ai_models/model/strategy.py
+++ ai_models/model/strategy.py
@@ -52,51 +52,25 @@ def exhaustive_feature_selection(data,
                                  algorithm,
                                  n_jobs=1,
                                  show_progress=False):
-    def initializer():
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-    feature_set = itertools.chain.from_iterable(
-        itertools.combinations(range(data[0].shape[1]), k + 1)
-        for k in range(data[0].shape[1]))
-    columns_set = ((data[0].iloc[:, list(sub)], data[1].iloc[:, list(sub)])
-                   for sub in feature_set)
-
-    res = []
-
-    logger.info("Starting %d parallel jobs", n_jobs)
     start_time = time.time()
-    n_sets = 2 ** data[0].shape[1]
-    with mp.Pool(n_jobs, initializer=initializer) as pool:
-        g = functools.partial(execute_model,
-                              labels=labels,
-                              algorithm=algorithm)
-        results = pool.imap_unordered(g, columns_set, chunksize=(300))
-        if show_progress:
-            results = tqdm.tqdm(results,
-                                desc="Features",
-                                total=n_sets,
-                                leave=False)
-        try:
-            for model, acc, training_time, cols in results:
-                pickled = codecs.encode(pickle.dumps(model, protocol=4),
-                                        "base64").decode()
-                # The following returns the saved model from a base64 string
-                # saved into the variable "pickled".
-                # >>> pickle.loads(codecs.decode(pickled.encode(), "base64"))
-                res.append({
-                    'model': pickled,
-                    'columns': cols,
-                    'accuracy': {
-                        'absolute': f'{acc[0]}/{len(labels[1])}',
-                        'relative': acc[1]
-                    },
-                    'time': training_time  # in seconds
-                })
-        except KeyboardInterrupt:
-            logger.warning("Received SIGINT! Stopping early")
-
+    res = {}
+    try:
+        sfs = mlx.feature_selection.ExhaustiveFeatureSelector(algorithm,
+                                                              cv=10,
+                                                              n_jobs=n_jobs,
+                                                              scoring='accuracy')
+        out = sfs.fit(data[0], labels[0])
+        # pickled = codecs.encode(pickle.dumps(model, protocol=4), "base64").decode()
+        # feature_set = [data[0].columns.get_loc(col) for col in cols]
+        res = {
+            # 'model': pickled,
+            'columns': out.best_feature_names_,
+            'score': out.best_score_,
+            'all': out.subsets_
+        }
+    except KeyboardInterrupt:
+        logger.warning("Received SIGINT! Stopping early")
     end_time = time.time()
-
     return res, end_time - start_time
 
 
