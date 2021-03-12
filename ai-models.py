906882b50f7331720020c304fbf516e7e98cc685
Andrea Esposito <esposito_andrea99@hotmail.com>
2021-03-12 09:54:23 +0100

    Solve warnings and errors

--- ai-models.py
+++ ai-models.py
@@ -14,7 +14,7 @@ import pandas as pd
 import sklearn as sk
 import yaml
 from mlxtend import feature_selection
-from sklearn import ensemble, svm, tree
+from sklearn import ensemble, svm, tree, neural_network
 
 logger = logging.getLogger("ai_models")
 
@@ -132,7 +132,6 @@ def load(path: pathlib.Path, emotion: str, width: int = None, location: str = No
 def test_report(data: Tuple[pd.DataFrame, pd.DataFrame], labels: Tuple[pd.Series, pd.Series],
                 algorithm: sk.base.ClassifierMixin) \
         -> Dict[str, Union[float, Dict[str, List[float]]]]:
-    output = {}
     test_algorithm_copy = sk.base.clone(algorithm)
     start_time = time.time()
     test_algorithm_copy.fit(data[0], labels[0])
@@ -146,37 +145,14 @@ def test_report(data: Tuple[pd.DataFrame, pd.DataFrame], labels: Tuple[pd.Series
     return output
 
 
-def execute_model(data: Tuple[pd.DataFrame, pd.DataFrame], labels: Tuple[pd.Series, pd.Series],
-                  algorithm: sk.base.ClassifierMixin) \
-        -> Dict[str, Union[float, Dict[str, List[float]]]]:
-    output = {}
-    cv_algorithm_copy = sk.base.clone(algorithm)
+def forward_feature_selection(data, labels, algorithm, n_jobs=1, show_progress=False):
     start_time = time.time()
-    output["cross_val"] = sk.model_selection.cross_validate(cv_algorithm_copy, data[0], labels[0], cv=CV_FOLD)
-    logger.debug("Completed cross validation")
-    cv_time = time.time() - start_time
-    # Convert all numpy's arras to Python's lists for better compatibility with other libraries (ie. YAML)
-    for key in output["cross_val"]:
-        output[key] = output[key].tolist()
-    output["cross_val"]["time"] = cv_time
-
-    output['final'] = test_report(data, labels, algorithm)
-
-    return output
-
-
-def forward_feature_selection(data,
-                              labels,
-                              algorithm,
-                              n_jobs=1,
-                              show_progress=False):
-    start_time = time.time()
-    res = {}
     sfs = mlx.feature_selection.SequentialFeatureSelector(algorithm,
                                                           k_features="best",
                                                           cv=10,
                                                           n_jobs=n_jobs,
-                                                          verbose=2, scoring='accuracy')
+                                                          verbose=2 if show_progress else 0,
+                                                          scoring='accuracy')
     out = sfs.fit(data[0], labels[0])
     logger.debug("Completed SFFS")
 
@@ -199,13 +175,8 @@ def forward_feature_selection(data,
     return res, end_time - start_time
 
 
-def exhaustive_feature_selection(data,
-                                 labels,
-                                 algorithm,
-                                 n_jobs=1,
-                                 show_progress=False):
+def exhaustive_feature_selection(data, labels, algorithm, n_jobs=1):
     start_time = time.time()
-    res = {}
     sfs = mlx.feature_selection.ExhaustiveFeatureSelector(algorithm,
                                                           cv=10,
                                                           n_jobs=n_jobs,
@@ -225,22 +196,32 @@ def exhaustive_feature_selection(data,
     return res, end_time - start_time
 
 
-def pca(data, labels, algorithm, n_jobs=1, show_progress=False):
+def pca(data, labels, algorithm):
     # Uses Minka's MLE - Thomas P. Minka, "Automatic choice of dimensionality for PCA", NIPS 2000
+    process_start_time =time.time()
     get_pca = sk.decomposition.PCA(n_components='mle')
     get_pca.fit(data[0])
     pca_output = (get_pca.transform(data[0]), get_pca.transform(data[1]))
+
+    res = {}
+
+    cv_algorithm_copy = sk.base.clone(algorithm)
     start_time = time.time()
-    res = execute_model(pca_output, labels, algorithm)
+    res["cross_val"] = sk.model_selection.cross_validate(cv_algorithm_copy, pca_output[0], labels[0], cv=CV_FOLD)
+    logger.debug("Completed cross validation")
+    cv_time = time.time() - start_time
+    # Convert all numpy's arras to Python's lists for better compatibility with other libraries (ie. YAML)
+    for key in res["cross_val"]:
+        res["cross_val"][key] = res["cross_val"][key].tolist()
+    res["cross_val"]["time"] = cv_time
+
+    res['final'] = test_report(pca_output, labels, algorithm)
     res["n_components"] = int(get_pca.n_components_)
-    end_time = time.time()
-    return res, end_time - start_time
+    return res, time.time() - process_start_time
 
 
 @click.command()
-@click.argument('dataset',
-                type=click.Path(exists=True, dir_okay=True, file_okay=False,
-                                path_type=pathlib.Path))
+@click.argument('dataset', type=click.Path(exists=True, dir_okay=True, file_okay=False))
 @click.argument('emotion',
                 type=click.Choice([e.split('.')[2] for e in KEYS_TO_PREDICT],
                                   case_sensitive=False))
@@ -248,20 +229,17 @@ def pca(data, labels, algorithm, n_jobs=1, show_progress=False):
 @click.argument('location', type=click.Choice(['before', 'after', 'full'],
                                               case_sensitive=False))
 @click.option('--random', '-r', type=int, default=0, help='Set the random seed')
-@click.option('--out', '-o', type=click.Path(dir_okay=False, file_okay=True,
-                                             path_type=pathlib.Path),
-              default=None,
+@click.option('--out', '-o', type=click.File(mode='w'), default='-',
               help="Set the path where the model will be saved. A new file with the"
                    " same name as the model's emotion will be created inside this"
                    " directory.")
-@click.option('--verbose', '-v', help='Run verbosely', is_flag=True)
+@click.option('--verbose', '-V', help='Run verbosely', is_flag=True)
 @click.option('--progress', '-P', help='Show progress', is_flag=True)
 @click.option('--jobs', '-j', type=int, default=1,
               help='Number of parallel jobs')
 @click.option('--algorithm', '-a',
               type=click.Choice(AVAILABLE_MODELS.keys(), case_sensitive=False),
-              default=[list(AVAILABLE_MODELS.keys())[0]], help='The algorithm',
-              multiple=True)
+              default=[list(AVAILABLE_MODELS.keys())[0]], help='The algorithm')
 @click.option('--strategy', '-s',
               type=click.Choice(['sfs', 'pca', 'efs'], case_sensitive=False),
               default=['sfs'], help='The strategy to apply', multiple=True)
@@ -279,12 +257,13 @@ def main(dataset, emotion, width, location, random, out, verbose, progress,
         )
 
     logger.info("Loading the dataset from '%s'", dataset)
-    data, labels = load(dataset, emotion, width=width, location=location)
+    data, labels = load(pathlib.Path(dataset), emotion, width=width, location=location)
     logger.info("Loaded a dataset of size %s", data.shape)
 
     if os.getenv('UXSAD_ENV') == 'test':
         logger.warning("Started in test mode")
         data = data.iloc[0:300, :]
+        labels = labels.iloc[0:300]
 
     logger.info("Splitting the dataset into train and test set (test ratio: %.2f%%)", 100 * 0.3)
     train_data, test_data, train_labels, test_labels = sk.model_selection.train_test_split(data, labels, train_size=0.7,
@@ -306,7 +285,6 @@ def main(dataset, emotion, width, location, random, out, verbose, progress,
         res['exhaustive_feature_selection'], duration = exhaustive_feature_selection(
             (train_data, test_data), (train_labels, test_labels),
             AVAILABLE_MODELS[algorithm],
-            show_progress=progress,
             n_jobs=jobs)
         logger.info("Forward feature selection completed. Took %.2f seconds",
                     duration)
@@ -314,11 +292,10 @@ def main(dataset, emotion, width, location, random, out, verbose, progress,
         logger.info("Starting PCA")
         res['pca'], duration = pca(
             (train_data, test_data), (train_labels, test_labels),
-            AVAILABLE_MODELS[algorithm],
-            show_progress=progress,
-            n_jobs=jobs)
+            AVAILABLE_MODELS[algorithm])
         logger.info("PCA completed. Took %.2f seconds", duration)
-    print(yaml.dump(res))
+
+    out.write(yaml.dump(res))
 
 
 if __name__ == '__main__':
