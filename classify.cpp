2b79fc28a5e03fe052411c166e7a8019133ac40a
Andrea Esposito <esposito_andrea99@hotmail.com>
2021-04-20 11:42:56 +0200

    Add TODO list and remove unused variable

--- classify.cpp
+++ classify.cpp
@@ -5,9 +5,16 @@
 #include <string>
 
 std::string get_step(const std::string&, int, int min=0, int max=100);
+
 int main()
 {
-    int ncls = 3;
+    // TODO: Add input file argument
+    // TODO: Add output file option
+    // TODO: Add number of classes option
+    // TODO: Add "help" option
+    // TODO: Add "version" option
+
+    const int ncls = 3;
     std::string line;
     std::getline(std::cin, line);
     std::cout << line << std::endl;
@@ -16,13 +23,15 @@ int main()
         std::stringstream sstream(line);
         std::string segment;
         std::vector<std::string> seglist;
+        // FIXME: We could have more columns: use the first line to know the
+        //        actual length of the array
         seglist.reserve(1235);
 
-        size_t i = 0;
         while(std::getline(sstream, segment, ',')) {
             seglist.push_back(segment);
         }
 
+        // FIXME: instead of hard-coded position, use the column names
         for(int i = 29; i <=38; ++i) {
           if(i==37)
             seglist[i] = get_step(seglist[i], ncls, -100, 100);
